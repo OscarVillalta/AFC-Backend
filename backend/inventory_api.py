a86295d760844c336474ae52a4c7399f64adb607
OscarOstorga <oscarvillalata@gmail.com>
2026-03-13 10:27:29 -0700

    Run run

--- backend/inventory_api.py
+++ backend/inventory_api.py
@@ -1,4 +1,4 @@
-import _bootstrap
+from . import _startup
 from app import create_app
 app = create_app()
-app.run(host="0.0.0.0", port=5000, debug=True)
+app.run(host="0.0.0.0", port=5000, debug=False)
