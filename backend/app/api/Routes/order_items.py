0541c4cbff32df71e813f51af85d3071c232f1eb
OscarOstorga <oscarvillalata@gmail.com>
2026-06-01 09:57:07 -0700

    Debug prints

--- backend/app/api/Routes/order_items.py
+++ backend/app/api/Routes/order_items.py
@@ -424,6 +424,10 @@ def allocate_remaining_order_items(item_id):
     if not qty_record:
         return jsonify({"error": "Quantity record not found"}), 404
     
+    print("ID: " + item.id)
+    print("Order type" + order.type)
+    print("Amount: " + qty_to_allocate)
+    
     if order.type == "incoming":
         qty_record.ordered += qty_to_allocate
     else:
